82f74211b6d9b6ddedb25be8a0e9b96a96440b5a
Orangerot <pink@orangerot.de>
2022-10-18 02:05:53 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Entropen.sh Matrix-Bot
+
+## Usage
+
+- create a new matrix account
+    - `settings -> help and informaiton -> copy access token`
+    - close element
+- `room -> room settings -> extended -> intern room id`
+
+Start the bot with the following environment vars set with your gathered
+information: 
+
+```sh
+MATRIX_PROVIDER=https://matrix.org
+MATRIX_ACCESS_TOKEN=your_token
+MATRIX_ROOM_ID=intern_room_id
+./entropen
+```
---
+++ entropen.sh
@@ -0,0 +1,24 @@
+if [ -z "$MATRIX_PROVIDER" ] || [ -z "$MATRIX_ACCESS_TOKEN" ] || [ -z "$MATRIX_ROOM_ID" ]
+then
+    echo 'env vars $MATRIX_PROVIDER or $MATRIX_ACCESS_TOKEN or $MATRIX_ROOM_ID not set'
+    exit -1
+fi
+
+provider="$MATRIX_PROVIDER"
+token="$MATRIX_ACCESS_TOKEN"
+room="$MATRIX_ROOM_ID"
+
+status=false
+
+while true
+do 
+    update="$(curl https://club.entropia.de | head -8 | tail -1 | tr -d ' ')"
+    if [ "$update" != "$status" ]
+    then 
+        status="$update"
+        curl -XPUT -d "{\"topic\": \"$update\"}" \
+            "$provider/_matrix/client/r0/rooms/$room/state/m.room.topic?access_token=$token"
+    fi
+    sleep 10
+done
+
